fd2e547104eb49fb0432b8137476793eeb41b2c0
Niedja92 <niedleticia@hotmail.com>
2023-12-11 14:29:06 -0300

    zatt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { api } from "../../services/api";
 
 const initialItens = [
   { id: 1, nome: "Banana" },
@@ -7,17 +8,50 @@ const initialItens = [
 ];
 
 export default function Home() {
+  const [textInput, setTexInput] = useState("")
   const [itens, setItens] = useState(initialItens);
 
   async function handleClick() {
-    const response = await fetch("http://192.168.68.154:3000/produtos");
-    const produtos = await response.json();
-    setItens(produtos);
-    console.log(produtos);
+    const response = await api.get("/produtos");
+    console.log(response);
+    setItens(response.data);
+    
+    //const response = await fetch("http://192.168.68.154:3000/produtos");
+    //const produtos = await response.json();
+    //setItens(produtos);
+    //console.log(produtos);
+  }
+
+ async function handleAddItem(){
+    console.log(textInput);
+    const data = { nome: textInput};
+
+    try {
+      const response = await fetch("http://192.168.68.154:3000/produtos", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+      alert ("Ocorreu um erro");
+    }
   }
 
   return (
     <main>
+
+      <div>
+        <input onChange={(e) => setTexInput(e.target.value)} placeholder="digite aq" />
+        <button onClick={handleAddItem}>enviar</button>
+      </div>
+
+
       <button onClick={handleClick}>Buscar informação no servidor</button>
 
       <ul>
